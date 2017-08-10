05c26d55ff76a51b5485b6bfe2c09c815312e09f
Shabitha <shabitha.raj@gmail.com>
2017-08-10 10:36:24 -0700

    Add files via upload

---
+++ ConvertsString.java
@@ -0,0 +1,16 @@
+
+public class ConvertsString {
+
+	public static void main(String[] args) {
+		//Write a program which accepts a string like "This is nice" and converts it to a string like "This1 is2 nice3"
+		
+		String str = "This is nice";
+		
+		for (int i = 0; i > str.length() ; i++){
+			
+			System.out.println(str+(i));
+		}
+
+	}
+
+}
